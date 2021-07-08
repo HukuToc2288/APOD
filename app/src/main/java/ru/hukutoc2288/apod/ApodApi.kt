df549b2931bcfa8286e434866784296741653286
HukuToc2288 <hukutoc2288@yandex.ru>
2021-07-08 22:12:05 +0300

    Now it can display today's image with description

--- app/src/main/java/ru/hukutoc2288/apod/ApodApi.kt
+++ app/src/main/java/ru/hukutoc2288/apod/ApodApi.kt
@@ -6,5 +6,8 @@ import retrofit2.http.Query
 
 interface ApodApi {
     @GET("/planetary/apod")
-    fun getData(@Query("api_key") apiKey: String): Call<List<ApodEntry>>
+    fun getToday(@Query("api_key") apiKey: String=API_KEY): Call<ApodEntry>
+
+    @GET("/planetary/apod")
+    fun getRandom(@Query("count") count: Int=1, @Query("api_key") apiKey: String=API_KEY): Call<List<ApodEntry>>
 }
--- app/src/main/java/ru/hukutoc2288/apod/MainActivity.kt
+++ app/src/main/java/ru/hukutoc2288/apod/MainActivity.kt
@@ -2,14 +2,23 @@ package ru.hukutoc2288.apod
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.format.DateUtils
+import android.transition.Visibility
 import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.core.widget.NestedScrollView
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import retrofit2.Retrofit
+import com.squareup.picasso.Picasso
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 
 lateinit var fab: FloatingActionButton
 lateinit var nestedScroll: NestedScrollView
@@ -17,6 +26,8 @@ lateinit var pictureView: ImageView
 lateinit var titleTextView: TextView
 lateinit var dateTextView: TextView
 lateinit var model: MainModel
+lateinit var pictureLoader: ProgressBar
+lateinit var descriptionTextView: TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +40,38 @@ class MainActivity : AppCompatActivity() {
         pictureView = findViewById(R.id.picture)
         titleTextView = findViewById(R.id.title)
         dateTextView = findViewById(R.id.date)
-        var response = apodApi.getData(API_KEY)
+        pictureLoader = findViewById(R.id.image_loader)
+        descriptionTextView = findViewById(R.id.description)
+
+        val dateFormat = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
+
+        var response = apodApi.getToday().enqueue(object : Callback<ApodEntry> {
+            override fun onResponse(call: Call<ApodEntry>?, response: Response<ApodEntry>?) {
+                val entry = response!!.body()
+                if (DateUtils.isToday(entry.date.time)) {
+                    dateTextView.text = getString(R.string.date_today)
+                } else {
+                    dateTextView.text = dateFormat.format(entry.date)
+                }
+                titleTextView.text = entry.title
+                descriptionTextView.text = entry.explanation
+
+                Picasso.get().load(entry.url).into(pictureView, object : com.squareup.picasso.Callback {
+                    override fun onSuccess() {
+                        pictureView.visibility = View.VISIBLE
+                        pictureLoader.visibility = View.GONE
+                    }
+
+                    override fun onError(e: Exception?) {
+
+                    }
+                })
+            }
+
+            override fun onFailure(call: Call<ApodEntry>?, t: Throwable) {
+                t.printStackTrace()
+            }
+
+        })
     }
 }
