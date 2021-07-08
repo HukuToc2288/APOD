85ca00bbe2c787622f9891cf579b61a2818f1304
HukuToc2288 <hukutoc2288@yandex.ru>
2021-07-08 18:16:32 +0300

    Converted FabScrollBehavior to Kotlin

--- app/src/main/java/ru/hukutoc2288/apod/FabScrollBehavior.kt
+++ app/src/main/java/ru/hukutoc2288/apod/FabScrollBehavior.kt
@@ -1,45 +1,44 @@
-package ru.hukutoc2288.apod;
+package ru.hukutoc2288.apod
 
-import android.content.Context;
-import android.content.res.TypedArray;
-import android.util.AttributeSet;
-import android.view.View;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
-import com.google.android.material.appbar.AppBarLayout;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-public class FabScrollBehavior extends CoordinatorLayout.Behavior<FloatingActionButton> {
-    private int toolbarHeight;
-
-    public FabScrollBehavior(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        this.toolbarHeight = getToolbarHeight(context);
+class FabScrollBehavior(context: Context, attrs: AttributeSet?) :
+    CoordinatorLayout.Behavior<FloatingActionButton>(context, attrs) {
+    private val toolbarHeight: Int
+    override fun layoutDependsOn(parent: CoordinatorLayout, fab: FloatingActionButton, dependency: View): Boolean {
+        return dependency is AppBarLayout
     }
 
-    @Override
-    public boolean layoutDependsOn(CoordinatorLayout parent, FloatingActionButton fab, View dependency) {
-        return dependency instanceof AppBarLayout;
-    }
-
-    @Override
-    public boolean onDependentViewChanged(CoordinatorLayout parent, FloatingActionButton fab, View dependency) {
-        if (dependency instanceof AppBarLayout) {
-            CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) fab.getLayoutParams();
-            int fabBottomMargin = lp.bottomMargin;
-            int distanceToScroll = fab.getHeight() + fabBottomMargin;
-            float ratio = (float)dependency.getY()/(float)toolbarHeight;
-            fab.setTranslationY(-distanceToScroll * ratio);
+    override fun onDependentViewChanged(
+        parent: CoordinatorLayout,
+        fab: FloatingActionButton,
+        dependency: View
+    ): Boolean {
+        if (dependency is AppBarLayout) {
+            val lp = fab.layoutParams as CoordinatorLayout.LayoutParams
+            val fabBottomMargin = lp.bottomMargin
+            val distanceToScroll = fab.height + fabBottomMargin
+            val ratio = dependency.getY() / toolbarHeight.toFloat()
+            fab.translationY = -distanceToScroll * ratio
         }
-        return true;
+        return true
     }
 
-    public static int getToolbarHeight(Context context) {
-        final TypedArray styledAttributes = context.getTheme().obtainStyledAttributes(
-                new int[]{R.attr.actionBarSize});
-        int toolbarHeight = (int) styledAttributes.getDimension(0, 0);
-        styledAttributes.recycle();
-
-        return toolbarHeight;
+    companion object {
+        fun getToolbarHeight(context: Context): Int {
+            val styledAttributes = context.theme.obtainStyledAttributes(intArrayOf(R.attr.actionBarSize))
+            val toolbarHeight = styledAttributes.getDimension(0, 0f).toInt()
+            styledAttributes.recycle()
+            return toolbarHeight
+        }
     }
 
+    init {
+        toolbarHeight = getToolbarHeight(context)
+    }
 }
